29c72a6b026de4030ccd2b858a84a55706cece42
Student <Student@DESKTOP-EF9PEQ2>
2019-03-14 13:14:33 +0900

    calendarExam finish

--- JavaStudy/src/ch04/CalendarExam.java
+++ JavaStudy/src/ch04/CalendarExam.java
@@ -10,6 +10,9 @@ public class CalendarExam {
 		int year = cal.get(Calendar.YEAR);
 		int month = cal.get(Calendar.MONTH) + 1;
 		int date = cal.get(Calendar.DATE);
+		
+		//달력의 날짜를 지정
+		cal.set(2012, 1, 1);
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
 		int lastDate = cal.getActualMaximum(Calendar.DATE);
 		
@@ -18,7 +21,7 @@ public class CalendarExam {
 		System.out.println("--------------------");
 		
 		//매월 1일의 시작  요일 맞춰주기(여백)
-		for(int i = 1; i <= dayOfWeek; i++) {
+		for(int i = 1; i < dayOfWeek; i++) {
 			System.out.print("   ");
 		}
 		
@@ -30,11 +33,11 @@ public class CalendarExam {
 				System.out.print(i + " ");
 			}
 			
-			dayOfWeek = dayOfWeek + 1;
 			
 			if(dayOfWeek % 7 == 0) {
 				System.out.println();
 			}
+			dayOfWeek = dayOfWeek + 1;
 		}
 		
 //		System.out.println(year);
