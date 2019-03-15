10df55adae6eef3bd35b2814cb85d66248f251ce
Student <Student@DESKTOP-EF9PEQ2>
2019-03-15 09:20:50 +0900

    exam5_3 fixed

--- JavaStudy/src/ch05/Exam5_3.java
+++ JavaStudy/src/ch05/Exam5_3.java
@@ -4,11 +4,11 @@ public class Exam5_3 {
 	public static void main(String[] args) {
 		char[] nums = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
 
-		char temp = 0;
-		for (int i = 0; i < nums.length; i++) {
+		    //char temp = 0;
+		for (int i = 0; i < 10000; i++) {
 			// 코드 작성		
 		    	int random = (int) (Math.random() * nums.length);
-
+		    	char temp = 0;
 				temp = nums[0];
 				nums[0] = nums[random];
 				nums[random] = temp;
