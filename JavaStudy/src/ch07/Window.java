f53dd50f3af33514ef40f3b877fd48eef68d19c9
Student <Student@DESKTOP-EF9PEQ2>
2019-03-20 17:36:54 +0900

    3/20 (수) 수업과정

--- JavaStudy/src/ch07/Window.java
+++ JavaStudy/src/ch07/Window.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class Window {
 	public static void main(String[] args) {
-		JFrame j = new JFrame();
+		JFrame j = new JFrame("³» Ã¢");
 		j.setSize(200, 100);
 		j.setVisible(true);
 	}
